e9da7f108721ff2865b2dd462178a53bc805d37f
Peter Jones <pjxcvi@proton.me>
2023-11-07 17:48:36 -0500

    add intersection algo

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,20 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+  const map = new Map();
+  const resultSet = new Set();
+
+  for (const e of a) {
+    map.set(e, true);
+  }
+
+  for (const e of b) {
+    if (map.has(e)) {
+      resultSet.add(e);
+    }
+  }
+
+  const intersectionArray = Array.from(resultSet);
+
+  return intersectionArray;
+}
 
 module.exports = intersection;
